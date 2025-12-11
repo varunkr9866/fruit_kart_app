784a87e6914a3bce1955b0965ec5825bee5b9a1a
Varun Kumar Reddy <varunkr9866@gmail.com>
2025-12-11 10:20:20 +0530

    WORKING ON HEADER STYLING

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import Header from './components/Header.jsx';
+import Header from './components/header/Header.jsx';
 import Signup from './components/SignUp.jsx';
 import Cart from './components/Cart.jsx';
 import {BrowserRouter as Router,Router,Route} from 'react-router-dom';
