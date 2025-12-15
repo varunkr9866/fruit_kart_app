64c04bd13024f6e90975c49b7971c4782981f0fd
Varun Kumar Reddy <varunkr9866@gmail.com>
2025-12-15 10:18:13 +0530

    COMPLETED PROJECT CLASS BASED APP

--- froentend_class_based/src/index.js
+++ froentend_class_based/src/index.js
@@ -1,12 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
     <App />
-
+  </BrowserRouter>
 );
-
